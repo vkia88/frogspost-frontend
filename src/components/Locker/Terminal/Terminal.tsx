01c27fec35382009be518ccc01e208d09bfdda7e
vkia88 <vkia88@inf.elte.hu>
2022-09-23 20:22:41 +0200

    deposit / pick up buttons

--- src/components/Locker/Terminal/Terminal.tsx
+++ src/components/Locker/Terminal/Terminal.tsx
@@ -1,3 +1,20 @@
+import { Button, ButtonGroup } from "@mui/material";
+import { useState } from "react";
+import { TerminalType } from "../../../constants/TerminalType";
+
 export default function Terminal() {
-  return <div>"terminal"</div>;
+  const [type, setType] = useState<TerminalType>(TerminalType.DEPOSIT);
+
+  return (
+    <>
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        <Button onClick={() => setType(TerminalType.DEPOSIT)}>Deposit</Button>
+        <Button onClick={() => setType(TerminalType.PICKUP)}>Pick Up</Button>
+      </ButtonGroup>
+      {type === TerminalType.DEPOSIT ? "Deposit" : "Pick Up"}
+    </>
+  );
 }
