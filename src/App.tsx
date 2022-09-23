b2b18303f8ab0b3b487cfb69c6d2b1d9fa8319af
vkia88 <vkia88@inf.elte.hu>
2022-09-23 19:46:59 +0200

    adding classnames

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
 import Locker from "./components/Locker/Locker";
+import classnames from "classnames";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={classnames("app")}>
       <Header />
       <Locker />
     </div>
