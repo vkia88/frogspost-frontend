c22b240a39e7b6743094128a2d670410e88a547b
vkia88 <vkia88@inf.elte.hu>
2022-09-26 02:03:35 +0200

    adding datepicker

--- src/components/Locker/Terminal/Deposit/Deposit.tsx
+++ src/components/Locker/Terminal/Deposit/Deposit.tsx
@@ -1,12 +1,20 @@
 import {
+  Button,
+  ButtonGroup,
   FormControl,
   FormControlLabel,
   FormLabel,
   Radio,
   RadioGroup,
+  TextField,
 } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import moment, { Moment } from "moment";
+import { useState } from "react";
 
 export default function Deposit() {
+  const [value, setValue] = useState<Moment | null>(moment());
+
   return (
     <>
       <h3>Deposit</h3>
@@ -17,6 +25,31 @@ export default function Deposit() {
           <FormControlLabel value="B" control={<Radio />} label="B" />
           <FormControlLabel value="C" control={<Radio />} label="C" />
         </RadioGroup>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel id="date-label">Until</FormLabel>
+        <DatePicker
+          label="Pickable Until"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+          minDate={moment()}
+          maxDate={moment().add(1, "year")}
+        />
+        <ButtonGroup variant="outlined" aria-label="outlined button group">
+          <Button onClick={() => setValue(moment().add(1, "day"))}>
+            1 day
+          </Button>
+          <Button onClick={() => setValue(moment().add(3, "days"))}>
+            3 days
+          </Button>
+          <Button onClick={() => setValue(moment().add(1, "week"))}>
+            1 week
+          </Button>
+        </ButtonGroup>
       </FormControl>
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </LocalizationProvider>
   </React.StrictMode>
 );
 
